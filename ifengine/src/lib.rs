d8166d6c6d92394e4d5361828b26cbdf73ec7652
squirreljetpacks <squirreljetpacks@gmail.com>
2025-12-08 03:43:10 -0500

    vendor egui_alignments, rename some folders

--- ifengine/src/lib.rs
+++ ifengine/src/lib.rs
@@ -3,10 +3,12 @@
 //! Other projects in this space include Twine and Inkle.
 //!
 //! The goal is to enjoy an effortless writing experience, together with all the benefits of the Rust ecosystem.
-
-//! To use this library, you write functions which produce [`Responses`](https://docs.rs/ifengine/latest/ifengine/core/enum.Response.html), eventualling resolving to a [`View`](https://docs.rs/ifengine/latest/ifengine/view/struct.View.html). The view corresponding to the current game state is retrieved by calling [`Game::view`](https://docs.rs/ifengine/latest/ifengine/core/struct.Game.html#method.view).
 //!
-//! A view is a sequence of [`Objects`](https://docs.rs/ifengine/latest/ifengine/view/enum.Object.html) which you can attach by calling the provided [elements and macros](https://docs.rs/ifengine/latest/ifengine/elements/index.html) within a function decorated by [`#[ifview]`](https://docs.rs/ifengine/latest/ifengine/attr.ifview.html).
+//! To use this library, you write functions which produce [`Responses`](https://docs.rs/ifengine/latest/ifengine/core/enum.Response.html), eventualling resolving to a [`View`](https://docs.rs/ifengine/latest/ifengine/view/struct.View.html).
+//! Such functions are called [`Pages`](https://docs.rs/ifengine/latest/ifengine/core/type.Page.html), and are decorated by the [`#[ifview]`](https://docs.rs/ifengine/latest/ifengine/attr.ifview.html) macro.
+//! A starting page is used to initialize the [`Game`](https://docs.rs/ifengine/latest/ifengine/core/struct.Game.html), which can then be called upon to yield its current view through [`Game::view`](https://docs.rs/ifengine/latest/ifengine/core/struct.Game.html#method.view), and updated by interacting with the view.
+//! A view consists of a sequence of [`Objects`](https://docs.rs/ifengine/latest/ifengine/view/enum.Object.html) which you can attach by calling the provided [elements and macros](https://docs.rs/ifengine/latest/ifengine/elements/index.html) within the page.
+
 
 pub mod core;
 mod errors;
